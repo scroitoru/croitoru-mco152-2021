31c43096e0b3baf384bc9dc1388b44de4a90d03c
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-02-09 10:54:36 -0500

    dictionary scrabble homework

---
+++ src/Scrabble.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.util.*;
+
+//Program to search if a word is present in the dictionary or not
+public class Scrabble {
+    public static boolean wordPresent(String word) {
+        try {
+            Scanner sc = new Scanner(new File("dictionary.txt"));
+            while (sc.hasNext()) {
+                String currWord = sc.next();
+                if (currWord.equals(word))
+                    return true;
+            }
+        }catch (Exception e){
+            System.out.println("Sorry, the following error occurred: " + e);
+            return false;
+        }
+        return false;
+    }
+}
---
+++ src/ScrabbleTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ScrabbleTest {
+    @Test
+    public void ifWordThere(){
+        Scrabble search = new Scrabble();
+        assertEquals(true, search.wordPresent("and"));
+    }
+
+    @Test
+    public void ifWordNotThere(){
+        Scrabble search = new Scrabble();
+        assertEquals(false, search.wordPresent("snh"));
+    }
+}
