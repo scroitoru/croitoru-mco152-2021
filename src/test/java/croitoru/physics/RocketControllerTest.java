551df36752b59112c0c52f46ed9992702196e02e
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-02-23 19:44:05 -0500

    added mockito

---
+++ src/test/java/croitoru/physics/RocketControllerTest.java
@@ -0,0 +1,21 @@
+package croitoru.physics;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class RocketControllerTest {
+
+    @Test
+    public void calculateRocket() {
+        //given
+        RocketController controller = new RocketController();
+
+        //when
+        controller.calculateRocket(null);
+
+        //then
+
+    }
+
+
+}
