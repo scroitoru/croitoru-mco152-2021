caec929c2baa1d72afc80132174d72db97d436f6
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-02-28 22:30:55 -0500

    gradle and mockito

--- src/test/java/croitoru/physics/RocketControllerTest.java
+++ src/test/java/croitoru/physics/RocketControllerTest.java
@@ -1,19 +1,44 @@
 package croitoru.physics;
 
-import junit.framework.TestCase;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 public class RocketControllerTest {
+
+    @BeforeClass
+    public static void beforeClass() {
+        com.sun.javafx.application.PlatformImpl.startup(()->{});
+    }
+
 
     @Test
     public void calculateRocket() {
         //given
         RocketController controller = new RocketController();
+        controller.velocityField = mock(TextField.class);
+        doReturn("71").when(controller.velocityField).getText();
+
+        controller.angleField = mock(TextField.class);
+        doReturn("35").when(controller.angleField).getText();
+
+        controller.secondsField = mock(TextField.class);
+        doReturn("3").when(controller.secondsField).getText();
+
+        controller.rocketCanvas = mock(RocketCanvas.class);
+        controller.locationLabel = mock(Label.class);
+        ActionEvent event = mock(ActionEvent.class);
 
         //when
-        controller.calculateRocket(null);
+        controller.calculateRocket(event);
 
         //then
+        verify(controller.locationLabel).setText("(174.48, 78.07)");
+        verify(controller.rocketCanvas).draw(any(Rocket.class));
 
     }
 
